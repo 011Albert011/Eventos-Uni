ac5c8fd722471fb6f6a25e4bd13d50efb4808dcd
Luis Emmanuel Ramiro Luna <luisemmanuelpueblagmail.com>
2026-02-16 20:50:16 -0600

    Formulario contacto terminado

--- js/jsdatosconts.js
+++ js/jsdatosconts.js
@@ -3,7 +3,6 @@ function guardar() {
     const nombre = document.querySelector("#nombre").value;
     const email = document.querySelector("#email").value;
     const mensaje = document.querySelector("#mensaje").value;
-    const resultado=document.querySelector("#resultConts");
     //Obtenemos todos los datos de la localstorage para ahora convertirlo de json string a objeto o creamos un arreglo vacio si este esta vacio 
     let datosObtenidos = JSON.parse(localStorage.getItem("misdatos")) || [];
     //Agregamos mas elementos a la lista con el metodo push
@@ -15,11 +14,11 @@ function guardar() {
         }
     )
     //Lo regresamos a string
-    const datosstorage=JSON.stringify(datosObtenidos)
+    const datosstorage = JSON.stringify(datosObtenidos)
     localStorage.setItem("misdatos", datosstorage)
     console.log(datosObtenidos);
-    resultado.innerHTML="Los datos han sido guardados correctamente"
+    alert("Los datos han sido guardados correctamente")
 }
-function buscarDato(){
-    
+function buscarDato() {
+
 }
