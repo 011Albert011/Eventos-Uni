e689aaaeec03c88f22148530eec9f389e823317f
Diego Oswaldo Meneses Vera <mv202339589@alm.buap.mx>
2026-02-25 22:59:57 -0600

    actualización para el nuevo index js

--- js/crear_evento.js
+++ js/crear_evento.js
@@ -10,25 +10,27 @@ document.addEventListener("DOMContentLoaded", () => {
   formEvento.addEventListener("submit", function (e) {
     e.preventDefault();
 
-    // Tomamos el valor del input (ej. "2026-02-26T22:19") y cambiamos la 'T' por un espacio
-    const fechaFormateada = document
-      .getElementById("fecha")
-      .value.replace("T", " ");
+    // 1. Tomamos el valor crudo del input (ej: "2026-03-15T20:00")
+    const fechaHoraCruda = document.getElementById("fecha").value;
 
-    // Creamos el objeto
+    // 2. Lo dividimos en un arreglo separando por la letra "T"
+    const partesFechaHora = fechaHoraCruda.split("T");
+
+    // partesFechaHora[0] tendrá la fecha: "2026-03-15"
+    // partesFechaHora[1] tendrá la hora: "20:00"
+
     const nuevoEvento = {
       id: Date.now(),
       titulo: document.getElementById("titulo").value,
       tipo: document.getElementById("tipo").value.toLowerCase(),
-      fecha: fechaFormateada, // Usamos la variable ya formateada
+      fecha: partesFechaHora[0], // Guardamos la fecha sola
+      hora: partesFechaHora[1], // Guardamos la hora sola
       sede: document.getElementById("sede").value,
       cupo: parseInt(document.getElementById("cupo").value),
       descripcion: document.getElementById("descripcion").value,
     };
 
-    // Leemos la lista de eventos
     let listaEventos = JSON.parse(localStorage.getItem("eventosU")) || [];
-
     listaEventos.push(nuevoEvento);
     localStorage.setItem("eventosU", JSON.stringify(listaEventos));
 
